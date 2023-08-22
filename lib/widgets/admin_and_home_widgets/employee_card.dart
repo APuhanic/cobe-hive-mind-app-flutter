023a9edd2bc6cd21f7b06040c0a22259ed119a67
APuhanic <puhanicantonio@gmail.com>
2023-08-22 10:40:13 +0200

    Added employee card expansion animation

--- lib/widgets/admin_and_home_widgets/employee_card.dart
+++ lib/widgets/admin_and_home_widgets/employee_card.dart
@@ -4,9 +4,10 @@ import 'package:cobe_hive_mobile_app/data/models/user.dart';
 import 'package:cobe_hive_mobile_app/data/constants/endpoints.dart';
 import 'package:cobe_hive_mobile_app/providers/network_providers/selected_user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class EmployeeCard extends ConsumerWidget {
+class EmployeeCard extends HookConsumerWidget {
   final User user;
   const EmployeeCard(
     this.user, {
@@ -15,89 +16,143 @@ class EmployeeCard extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final isExpanded = useState(false);
+    final expansionAnimationController = useAnimationController(
+      duration: const Duration(milliseconds: 300),
+    );
+
+    final expansionAnimation = Tween<double>(begin: 120.0, end: 200.0).animate(
+      CurvedAnimation(
+        parent: expansionAnimationController,
+        curve: Curves.easeOut,
+      ),
+    );
+
     return GestureDetector(
       onTap: () {
         ref.read(selectedUserProvider.notifier).state = user;
         Navigator.pushNamed(context, '/public-profile-screen');
       },
+      onLongPress: () {
+        isExpanded.value = !isExpanded.value;
+        if (isExpanded.value) {
+          expansionAnimationController.forward();
+        } else {
+          expansionAnimationController.reverse();
+        }
+      },
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 18),
-        child: Container(
-          height: 120,
-          decoration: BoxDecoration(
-            color: AppColors.widgetBackground,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: const [
-              BoxShadow(
-                color: AppColors.shadow,
-                blurRadius: 10.0,
-              ),
-            ],
-          ),
-          child: Row(
-            children: [
-              Stack(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 18),
-                    child: CircleAvatar(
-                      radius: 40,
-                      backgroundImage: NetworkImage(
-                        '${Endpoints.baseUrl}${user.imageUrl}',
-                      ),
-                    ),
-                  ),
-                  if (user.status == UserStatus.online)
-                    Positioned(
-                      top: 5,
-                      right: 25,
-                      child: Container(
-                        width: 18,
-                        height: 18,
-                        decoration: const BoxDecoration(
-                          color: Colors.white,
-                          shape: BoxShape.circle,
-                        ),
-                        child: Center(
-                          child: Container(
-                            width: 14,
-                            height: 14,
-                            decoration: const BoxDecoration(
-                              color: AppColors.green,
-                              shape: BoxShape.circle,
-                            ),
+        child: AnimatedBuilder(
+          animation: expansionAnimation,
+          builder: (context, child) => Container(
+            height: expansionAnimation.value,
+            decoration: BoxDecoration(
+              color: AppColors.widgetBackground,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: const [
+                BoxShadow(
+                  color: AppColors.shadow,
+                  blurRadius: 10.0,
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    _UserImage(user: user),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${user.name} ${user.surname}',
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                            color: AppColors.text,
                           ),
                         ),
-                      ),
+                        const SizedBox(height: 5),
+                        Text(
+                          user.status!.name.toString().capitalize(),
+                          style: const TextStyle(
+                            fontSize: 15,
+                            color: AppColors.textAccent,
+                          ),
+                        ),
+                      ],
                     ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '${user.name} ${user.surname}',
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700,
-                      color: AppColors.text,
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  Text(
-                    user.status!.name.toString().capitalize(),
-                    style: const TextStyle(
-                      fontSize: 15,
-                      color: AppColors.textAccent,
-                    ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+                isExpanded.value && expansionAnimationController.isCompleted
+                    ? Column(
+                        children: [
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text('View profile'),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text('Send message'),
+                          ),
+                        ],
+                      )
+                    : const SizedBox(),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class _UserImage extends StatelessWidget {
+  const _UserImage({
+    required this.user,
+  });
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18),
+          child: CircleAvatar(
+            radius: 40,
+            backgroundImage: NetworkImage(
+              '${Endpoints.baseUrl}${user.imageUrl}',
+            ),
+          ),
+        ),
+        if (user.status == UserStatus.online)
+          Positioned(
+            top: 5,
+            right: 25,
+            child: Container(
+              width: 18,
+              height: 18,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+              ),
+              child: Center(
+                child: Container(
+                  width: 14,
+                  height: 14,
+                  decoration: const BoxDecoration(
+                    color: AppColors.green,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
